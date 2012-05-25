d5e6f5c1446b3caa6a1ed2c4e9cfbd9c49cb216d
Luke Lappin <lukelappin@gmail.com>
2012-05-24 20:33:01 -0400

    renamed initial files

---
+++ memmee-service/src/main/java/com/memmee/MemmeeDAO.java
@@ -0,0 +1,46 @@
+package com.memmee;
+
+import com.memmee.model.User;
+import com.memmee.model.UserMapper;
+import org.skife.jdbi.v2.sqlobject.Bind;
+import org.skife.jdbi.v2.sqlobject.SqlQuery;
+import org.skife.jdbi.v2.sqlobject.SqlUpdate;
+import org.skife.jdbi.v2.sqlobject.customizers.Mapper;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: waparrish
+ * Date: 5/2/12
+ * Time: 2:08 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface MemmeeDAO {
+
+    @SqlQuery("select * from user")
+    @Mapper(UserMapper.class)
+    List<User> findAll();
+
+    @SqlUpdate("insert into user (id, firstName, lastName, email) values (:id, :firstName, :lastName, :email)")
+    void insert(
+        @Bind("id") Long id
+        ,@Bind("firstName") String firstName
+        ,@Bind("lastName") String lastName
+        ,@Bind("email") String email
+    );
+
+    @SqlUpdate("update user set firstName = :firstName, lastName = :lastName, email = :email where id = :id")
+    int update(
+        @Bind("id") Long id
+        ,@Bind("firstName") String firstName
+        ,@Bind("lastName") String lastName
+        ,@Bind("email") String email
+    );
+
+    @SqlUpdate("delete from user where id = :id")
+    void delete(
+        @Bind("id") Long id
+    );
+
+}
---
+++ memmee-service/src/main/java/com/memmee/MemmeeService.java
@@ -0,0 +1,36 @@
+package com.memmee;
+
+import com.yammer.dropwizard.Service;
+import com.yammer.dropwizard.config.Environment;
+import com.yammer.dropwizard.db.Database;
+import com.yammer.dropwizard.db.DatabaseFactory;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: waparrish
+ * Date: 5/2/12
+ * Time: 11:49 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MemmeeService extends Service<MemmeeConfiguration> {
+
+    public static void main(String[] args) throws Exception
+    {
+        new MemmeeService().run(args);
+    }
+
+    private MemmeeService()
+    {
+        super("user");
+    }
+
+
+    @Override
+    protected void initialize(MemmeeConfiguration userConfiguration, Environment environment) throws Exception {
+
+        final DatabaseFactory factory = new DatabaseFactory(environment);
+        final Database db = factory.build(userConfiguration.getDatabase(), "mysql");
+        final MemmeeDAO dao = db.onDemand(MemmeeDAO.class);
+        environment.addResource(new MemmeeResource(dao));
+    }
+}
