ff5ae2cb1772f36eb38120eca2ffffbb3c3283a3
Luke Lappin <lukelappin@gmail.com>
2012-05-31 18:24:22 -0400

    added Theme

---
+++ memmee-service/src/main/java/com/memmee/theme/dto/Theme.java
@@ -0,0 +1,43 @@
+package com.memmee.theme.dto;
+
+import java.io.Serializable;
+
+
+public class Theme implements Serializable{
+	
+	private static final long serialVersionUID = 1706390233616761884L;
+
+	private Long id;
+	
+	private String name;
+	
+	private String stylePath;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getStylePath() {
+		return stylePath;
+	}
+
+	public void setStylePath(String stylePath) {
+		this.stylePath = stylePath;
+	}
+
+			    
+			   
+
+}
