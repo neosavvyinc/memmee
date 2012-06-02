ea2a334b95d29580d1d1be0d802b74d9ea5d236f
Luke Lappin <lukelappin@gmail.com>
2012-06-02 00:13:03 -0400

    added get by own id method

--- memmee-service/src/main/java/com/memmee/theme/dao/ThemeDAO.java
+++ memmee-service/src/main/java/com/memmee/theme/dao/ThemeDAO.java
@@ -4,16 +4,19 @@ import org.skife.jdbi.v2.sqlobject.Bind;
 import org.skife.jdbi.v2.sqlobject.SqlQuery;
 import org.skife.jdbi.v2.sqlobject.SqlUpdate;
 import org.skife.jdbi.v2.sqlobject.customizers.Mapper;
-import com.memmee.attachment.dto.AttachmentMapper;
 import com.memmee.theme.dto.Theme;
 import com.memmee.theme.dto.ThemeMapper;
 
 public interface ThemeDAO {
 	
+	
+	@SqlQuery("select * from theme where id = :id")
+    @Mapper(ThemeMapper.class)
+    Theme getTheme(@Bind("id") Long id);
 
     @SqlQuery("select * from theme where memmeeId = :memmeeId")
     @Mapper(ThemeMapper.class)
-    Theme getTheme(@Bind("memmeeId") Long memmeeId);
+    Theme getThemeByMemmeeId(@Bind("memmeeId") Long memmeeId);
 
     @SqlUpdate("insert into theme (id, memmeeId, name, stylePath) values (:id, :memmeeId, :name, :stylePath)")
     void insert(
