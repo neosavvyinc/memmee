c96b4e0625794ae6b187033c5ef4bfa987abeb81
Adam Parrish <aparrish@neosavvy.com>
2012-06-18 18:49:35 -0400

    Adding a NavigationController. Making sure that selected Home, New, View changes the Route and the Navigation controllers selected value and updates the URL.

--- memmee-client/src/main/resources/js/controller.js
+++ memmee-client/src/main/resources/js/controller.js
@@ -1,3 +1,28 @@
+function NavigationController($scope) {
+
+    $scope.navigationItems = [
+            { displayName: "Home", navigationLink: "#home", selected: "active" },
+            { displayName: "View", navigationLink: "#view", selected: "" },
+            { displayName: "New", navigationLink: "#new", selected: "" }
+        ];
+
+    $scope.select = function( $selectedNavigationLink ) {
+        for ( navIndex in $scope.navigationItems )
+        {
+            if( $scope.navigationItems[navIndex].navigationLink == $selectedNavigationLink.navigationLink )
+            {
+                $scope.navigationItems[navIndex].selected = "active";
+            }
+            else
+            {
+                $scope.navigationItems[navIndex].selected = "";
+            }
+        }
+    }
+
+}
+
+
 function RegistrationController($scope, $http) {
 
     $scope.user = {
