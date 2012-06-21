74feca8d9bbc78bee7a296710bbe8206f1b0fcc9
Adam Parrish <aparrish@neosavvy.com>
2012-06-21 19:40:58 -0400

    Updating a unit test.

--- memmee-service/src/test/resources/restTests/memmee/insertMemmee.sh
+++ memmee-service/src/test/resources/restTests/memmee/insertMemmee.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
-#curl -vvvvv \
-# -XPOST \
-# -HAccept:application/json \
-# -HContent-Type:application/json \
-# -d @memmee.json \
-# -d @attachment.json \
-# http://localhost:8080/memmeerest/insertmemmee/?apiKey=84104fe7-83c6-45db-abf2-01a5a389398f
+curl -vvvvv \
+    -XPOST \
+    -HAccept:application/json \
+    -HContent-Type:application/json \
+    -d @memmee.json \
+    http://localhost:8080/memmeerest/insertmemmee/?apiKey=test
 
-/usr/local/bin/wget -vvvvv \
- --header='Accept:application/json' \
- --header='Content-Type:application/json' \
- --post-file=memmee.json \
- http://localhost:8080/memmeerest/insertmemmee/?apiKey=84104fe7-83c6-45db-abf2-01a5a389398f
+#/usr/bin/wget -vvvvv \
+# --header='Accept:application/json' \
+# --header='Content-Type:application/json' \
+# --post-file=memmee.json \
+# http://localhost:8080/memmeerest/insertmemmee/?apiKey=test
