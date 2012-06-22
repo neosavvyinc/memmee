85d7401b041f51b27ce572b1fc4611bcdc0aeb8a
lukelappin <lukelappin@gmail.com>
2012-06-22 17:03:02 -0400

    added insert user test

--- memmee-service/src/test/resources/restTests/user/insertUser.sh
+++ memmee-service/src/test/resources/restTests/user/insertUser.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
-#curl -vvvvv \
-# -XPOST \
-# -HAccept:application/json \
-# -HContent-Type:application/json \
-# -d @memmee.json \
-# -d @attachment.json \
-# http://localhost:8080/memmeerest/insertmemmee/?apiKey=84104fe7-83c6-45db-abf2-01a5a389398f
+curl -vvvvv \
+ -XPOST \
+ -HAccept:application/json \
+ -HContent-Type:application/json \
+ -d @user.json \
+ http://localhost:8080/memmeeuserrest/user/
 
-/usr/local/bin/wget -vvvvv \
- --header='Accept:application/json' \
- --header='Content-Type:application/json' \
- --post-file=registration.json \
- http://local.memmee.com/memmeeuserrest/user
+
+
