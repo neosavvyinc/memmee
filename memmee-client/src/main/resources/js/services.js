2c77688350e285d7505966cc425546e105ae931a
Adam Parrish <aparrish@neosavvy.com>
2012-07-02 19:27:56 -0400

    Forwards you to /view if you are logged in and you refresh the /home screen.

--- memmee-client/src/main/resources/js/services.js
+++ memmee-client/src/main/resources/js/services.js
@@ -1,5 +1,5 @@
 var memmeeServices = angular.module('memmee-app.services', ['ngResource']);
-memmeeServices.factory("memmeeBroadCastService", function($rootScope) {
+memmeeServices.factory("memmeeBroadCastService", function($rootScope, $location) {
 
     var broadCastService = {};
 
@@ -9,6 +9,7 @@ memmeeServices.factory("memmeeBroadCastService", function($rootScope) {
     broadCastService.loginUser = function ( $user ) {
         this.user = $user;
         $rootScope.$broadcast('handleLogin');
+        $location.url( "/view" );
     }
 
     broadCastService.logoutUser = function( $user ) {
