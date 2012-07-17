a51be0ecc0be0e8b37f20d77304bc51950ca2dc8
Trevor Ewen <trevorewen@gmail.com>
2012-07-16 21:13:44 -0400

    Made memmees selectable in the archive list.

--- memmee-client/src/main/resources/js/view_mode_controller.js
+++ memmee-client/src/main/resources/js/view_mode_controller.js
@@ -25,12 +25,20 @@ function ViewModeController($scope, $http, broadCastService) {
             });
     };
 
-    $scope.getDefaultMemmee();
+    //Broadcast Handlers
+    $scope.$on('memmeeSelectedArchiveListController', function(event, memmee) {
+        $scope.memmee = memmee;
+    });
 
+    //UI
     $scope.getDisplayDate = function( )
     {
         return $scope.memmee.displayDate.toDateString();
     }
+
+    //Initializaton
+    $scope.getDefaultMemmee();
+
 }
 
 ViewModeController.$inject = ['$scope', '$http', 'memmeeBroadCastService'];
