5655cc327adc294b9bf7c5bc8d303670a090af1e
Trevor Ewen <trevorewen@gmail.com>
2012-08-01 22:09:01 -0400

    Left and right scrolling added for the buttons on the archive list.

--- memmee-client/src/main/resources/js/archive_list_controller.js
+++ memmee-client/src/main/resources/js/archive_list_controller.js
@@ -1,6 +1,9 @@
 function ArchiveListController($scope, $http, broadCastService) {
     $scope.user = broadCastService.user;
     $scope.memmees = null;
+    $scope.scrollableLeft = false;
+    $scope.scrollableRight = true;
+    $scope.amountScrolledRight = 0;
 
     //Action Handlers
     $scope.onMemmeeSelect = function (memmee) {
@@ -48,6 +51,28 @@ function ArchiveListController($scope, $http, broadCastService) {
             return MemmeeDateUtil.shortMonth(myDate.getMonth()) + " " + myDate.getDate().toString();
         }
         return null;
+    };
+
+    $scope.leftScroll = function (clazz) {
+        var scrollable = $scope.amountScrolledRight >= 300;
+        $scope.animateScroll($(clazz), "+=300px", scrollable, true)
+    };
+
+    $scope.rightScroll = function (clazz) {
+        var scrollable = $scope.amountScrolledRight < (300 * $scope.memmees.length - 700);
+        $scope.animateScroll($(clazz), "-=300px", scrollable, false);
+    };
+
+    $scope.animateScroll = function (obj, param, scrollable, left) {
+        if (scrollable) {
+            obj.animate({"margin-left":param}, 400, function () {
+                if (left) {
+                    $scope.amountScrolledRight -= 300;
+                } else {
+                    $scope.amountScrolledRight += 300;
+                }
+            });
+        }
     };
 
     //Initialization
