1c04d5016e6ab4fd01f0271677c8b8047b78e737
Adam Parrish <aparrish@neosavvy.com>
2012-08-26 18:02:01 -0400

    Setting up the logged out view - this obviously will be replaced when we know what it is supposed to look like.

---
+++ memmee-client/src/main/resources/js/services.js
@@ -0,0 +1,164 @@
+var memmeeServices = angular.module('memmee-app.services', ['ngResource']);
+memmeeServices.factory("memmeeBroadCastService", function ($rootScope, $location) {
+
+    var broadCastService = {};
+
+    broadCastService.user = null;
+    broadCastService.attachment = null;
+    broadCastService.apiKey = $location.search().apiKey;
+    broadCastService.selectedMemmee = null;
+
+    broadCastService.loginUser = function ($user) {
+        this.user = $user;
+        $user.password.value = "";
+        localStorage.setItem("user", JSON.stringify($user));
+        $rootScope.$broadcast(LoginControllerEvents.get('LOGIN'));
+
+        if ($location.url() == "/home" || $location.url() == "/requiredchangepassword") {
+            if ($user.password.temp)
+                $location.url("/requiredchangepassword");
+            else
+                $location.url("/loggedin");
+        }
+    }
+
+    broadCastService.logoutUser = function ($user) {
+        this.user = null;
+        localStorage.removeItem("user");
+        $location.path('/home');
+        $rootScope.$broadcast(LoginControllerEvents.get('LOGOUT'));
+    }
+
+    broadCastService.attachmentSuccess = function ($attachment) {
+        this.attachment = $attachment;
+        $rootScope.$broadcast('attachmentUploadSuccess');
+    }
+
+    broadCastService.deleteAttachment = function () {
+        $rootScope.$broadcast('deleteAttachment');
+    }
+
+    broadCastService.deleteAttachmentSuccess = function () {
+        $rootScope.$broadcast('deleteAttachmentSuccess');
+    }
+
+    var createMode = true;
+
+    broadCastService.isCreateMode = function () {
+        return createMode;
+    }
+
+
+    /**
+     * Modal Popup Notifications:
+     */
+    broadCastService.showProfileUpdatedSuccess = function () {
+        $rootScope.$broadcast('showProfileUpdatedSuccess');
+    }
+
+    broadCastService.showProfileUpdatedError = function () {
+        $rootScope.$broadcast('showProfileUpdatedError');
+    }
+
+    /**
+     * AlertsController
+     */
+    broadCastService.yesSelectedAlertsController = function (promptingEvent) {
+        $rootScope.$broadcast(AlertsControllerEvents.get('YES_SELECTED'), promptingEvent);
+    };
+
+    broadCastService.noSelectedAlertsController = function (promptingEvent) {
+        $rootScope.$broadcast(AlertsControllerEvents.get('NO_SELECTED'), promptingEvent);
+    };
+
+    /**
+     * ArchiveListController
+     */
+    broadCastService.memmeeSelectedArchiveListController = function (memmee) {
+        $rootScope.$broadcast(ArchiveListControllerEvents.get('MEMMEE_SELECTED'), memmee);
+    };
+
+    /**
+     * CreateModeController
+     */
+    broadCastService.confirmDiscardCreateModeController = function () {
+        $rootScope.$broadcast(CreateModeControllerEvents.get('CONFIRM_DISCARD'));
+    }
+
+    broadCastService.memmeeCreatedCreateModeController = function () {
+        $rootScope.$broadcast(CreateModeControllerEvents.get('MEMMEE_CREATED'));
+    }
+
+    broadCastService.createModeCancelledCreateModeController = function () {
+        createMode = false;
+        $rootScope.$broadcast(CreateModeControllerEvents.get('CREATE_MODE_CANCELLED'));
+    }
+
+    broadCastService.createModeStartedCreateModeController = function () {
+        createMode = true;
+        $rootScope.$broadcast(CreateModeControllerEvents.get('CREATE_MODE_STARTED'));
+    }
+
+    /**
+     * LoginController
+     */
+    broadCastService.invalidLoginLoginController = function () {
+        $rootScope.$broadcast(LoginControllerEvents.get('INVALID_LOGIN'));
+    };
+
+    /**
+     * ProfileController
+     */
+    broadCastService.showProfileUpdatedSuccess = function () {
+        $rootScope.$broadcast(ProfileControllerEvents.get('UPDATE_SUCCESS'));
+    }
+
+    broadCastService.showProfileUpdatedError = function () {
+        $rootScope.$broadcast(ProfileControllerEvents.get('UPDATE_FAILURE'));
+    }
+
+    /**
+     * RegistrationController
+     */
+    broadCastService.errorSavingUserRegistrationController = function (message) {
+        $rootScope.$broadcast(RegistrationControllerEvents.get('ERROR_SAVING'), message);
+    }
+
+    /**
+     * ViewModeController
+     */
+    broadCastService.confirmDeleteViewModeController = function () {
+        $rootScope.$broadcast(ViewModeControllerEvents.get('CONFIRM_DELETE'));
+    }
+
+    broadCastService.memmeeDeletedViewModeController = function () {
+        broadCastService.selectedMemmee = null;
+        $rootScope.$broadcast(ViewModeControllerEvents.get('MEMMEE_DELETED'));
+    }
+
+    broadCastService.showShareLinkViewModeController = function (memmee) {
+        $rootScope.$broadcast(ViewModeControllerEvents.get('SHOW_SHARE_LINK'), [memmee])
+    }
+
+    /**
+     * Initialization
+     */
+    broadCastService.setTheme = function (number) {
+        switch (number) {
+            case 0:
+                $rootScope.$broadcast('changeToDefaultTheme');
+                break;
+            case 1:
+                $rootScope.$broadcast('changeToTheme1');
+                break;
+            case 2:
+                $rootScope.$broadcast('changeToTheme2');
+                break;
+            case 3:
+                $rootScope.$broadcast('changeToTheme3');
+                break;
+        }
+    }
+
+    return broadCastService;
+});
