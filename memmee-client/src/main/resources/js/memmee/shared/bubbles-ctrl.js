6a0e3fc1833af937068070fd7c51687f285f881a
Trevor Ewen <trevorewen@gmail.com>
2012-09-06 00:47:16 -0400

    Completely implemented conversation bubble in create mode.

--- memmee-client/src/main/resources/js/memmee/shared/bubbles-ctrl.js
+++ memmee-client/src/main/resources/js/memmee/shared/bubbles-ctrl.js
@@ -1,16 +1,32 @@
 function BubblesController($scope, $http, broadCastService, $location) {
 
     //Handlers
-    $scope.toggleNewUserCreateMode = function() {
+    $scope.toggleNewUserCreateMode = function () {
         $scope.showNewUserCreateMode = !$scope.showNewUserCreateMode;
     };
 
+    $scope.clearALlBubbles = function() {
+        $scope.showNewUserCreateMode = false;
+    };
+
+    //Styles
+    $scope.setNewUserCreateModeStyle = function () {
+        $scope.newUserCreateModeStyle = {
+            'top':"270px",
+            'left':((window.innerWidth / 2) + 185).toString() + "px"};
+        $scope.$apply();
+    }
+
     //Listeners
     $scope.$on(CreateModeControllerEvents.get('NEW_USER_LOGIN'), $scope.toggleNewUserCreateMode);
+    $scope.$on(LoginControllerEvents.get('LOGOUT'), $scope.clearALlBubbles);
+    $scope.$on(CreateModeControllerEvents.get('CREATE_MODE_CANCELLED'), $scope.clearALlBubbles);
+    window.addEventListener('onresize', $scope.setNewUserCreateModeStyle, false);
+    window.addEventListener('resize', $scope.setNewUserCreateModeStyle, false);
 
     //Initialization
     $scope.showNewUserCreateMode = false;
-
+    $scope.setNewUserCreateModeStyle();
 }
 
 BubblesController.$inject = ['$scope', '$http', 'memmeeBroadCastService', '$location'];
