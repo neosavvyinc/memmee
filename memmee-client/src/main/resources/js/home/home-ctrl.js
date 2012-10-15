c994143a9cc6c3f19f9f540dd3af336682102f28
Adam Parrish <aparrish@neosavvy.com>
2012-10-14 21:45:34 -0400

    Introducing the ability to transition from one item to the next on the home page. Still lacks transition.

--- memmee-client/src/main/resources/js/home/home-ctrl.js
+++ memmee-client/src/main/resources/js/home/home-ctrl.js
@@ -1,14 +1,70 @@
-function HomeController($scope, $http, broadCastService) {
+function HomeController($scope) {
+    "use strict";
+    $scope.img1zindex = {"z-index": 3};
+    $scope.img2zindex = {"z-index": 2};
+    $scope.img3zindex = {"z-index": 1};
+
+    $scope.carouselState = "1";
 
     //Super/Inherited Methods
     DefaultController($scope,
         /* load/tearDown */ function () {
         });
 
-    //UI Initialization
-    $scope.initializeScrolling = function (clazz) {
-        $(clazz).scrollable();
-    };
+
+
+    $scope.clickNext = function () {
+        console.log("Clicking next");
+
+        if ($scope.carouselState < 3) {
+            $scope.carouselState++;
+        }
+
+        switch ($scope.carouselState) {
+        case 1:
+            $scope.img1zindex = {"z-index": 3};
+            $scope.img2zindex = {"z-index": 2};
+            $scope.img3zindex = {"z-index": 1};
+            break;
+        case 2:
+            $scope.img1zindex = {"z-index": 1};
+            $scope.img2zindex = {"z-index": 3};
+            $scope.img3zindex = {"z-index": 2};
+            break;
+
+        case 3:
+            $scope.img1zindex = {"z-index": 1};
+            $scope.img2zindex = {"z-index": 2};
+            $scope.img3zindex = {"z-index": 3};
+            break;
+        }
+    }
+
+    $scope.clickPrevious = function () {
+        console.log("Clicking previous");
+
+        if ($scope.carouselState > 1) {
+            $scope.carouselState--;
+        }
+
+        switch ($scope.carouselState) {
+        case 1:
+            $scope.img1zindex = {"z-index": 3};
+            $scope.img2zindex = {"z-index": 2};
+            $scope.img3zindex = {"z-index": 1};
+            break;
+        case 2:
+            $scope.img1zindex = {"z-index": 1};
+            $scope.img2zindex = {"z-index": 3};
+            $scope.img3zindex = {"z-index": 2};
+            break;
+        case 3:
+            $scope.img1zindex = {"z-index": 1};
+            $scope.img2zindex = {"z-index": 2};
+            $scope.img3zindex = {"z-index": 3};
+            break;
+        }
+    }
 }
 
-HomeController.$inject = ['$scope', '$http', 'memmeeBroadCastService'];
+HomeController.$inject = ['$scope'];
