d139c24980a82a0de34ca95ac3fe1ac9815ee9ad
Adam Parrish <aparrish@neosavvy.com>
2013-02-19 22:01:38 -0500

    Adding some directions on installing mysql from homebrew. Also fixing initial users. Also adding an nginx.conf file.

--- memmee-db/src/main/db/changelogs/initial-users.sql
+++ memmee-db/src/main/db/changelogs/initial-users.sql
@@ -4,3 +4,8 @@ GRANT ALL PRIVILEGES ON memmee.* TO 'memmee'@'%' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON memmee.* TO 'memmee'@'localhost' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON memmeetest.* TO 'memmee'@'%' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON memmeetest.* TO 'memmee'@'localhost' WITH GRANT OPTION;
+
+CREATE USER 'memmeetest'@'%' IDENTIFIED BY 'memmeetest';
+CREATE USER 'memmeetest'@'localhost' IDENTIFIED BY 'memmeetest';
+GRANT ALL PRIVILEGES ON memmeetest.* TO 'memmeetest'@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON memmeetest.* TO 'memmeetest'@'localhost' WITH GRANT OPTION;
