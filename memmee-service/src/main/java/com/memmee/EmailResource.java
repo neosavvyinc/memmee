2e8eaa06ff25cc8f32ca07a05addf45cecbc0a10
Adam Parrish <aparrish@neosavvy.com>
2013-04-15 23:32:08 -0400

    Fixing a bug in attachment uploading for email to memmee

--- memmee-service/src/main/java/com/memmee/EmailResource.java
+++ memmee-service/src/main/java/com/memmee/EmailResource.java
@@ -79,23 +79,12 @@ public class EmailResource {
 
         byte byteData[] = md.digest();
 
-        //convert the byte to hex format method 1
         StringBuffer sb = new StringBuffer();
         for (int i = 0; i < byteData.length; i++) {
             sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
         }
 
-        System.out.println("Hex format : " + sb.toString());
-
-        //convert the byte to hex format method 2
-        StringBuffer hexString = new StringBuffer();
-        for (int i=0;i<byteData.length;i++) {
-            String hex=Integer.toHexString(0xff & byteData[i]);
-            if(hex.length()==1) hexString.append('0');
-            hexString.append(hex);
-        }
-
-        return hexString.toString();
+        return sb.toString();
     }
 
     public static int checkForEmail(String mailbox) throws MessagingException, IOException {
@@ -217,8 +206,8 @@ public class EmailResource {
                                                     generateUniqueFileName(part.getFileName().toLowerCase() + (new Date()).toString());
 
                                             writeToFile(is, uploadedFileLocationToWrite);
-                                            String uploadedThumbFileLocation = writeThumbnailImage(uploadedFileLocationToWrite);
-                                            attachmentId = attachmentDao.insert(uploadedFileLocationToWrite, uploadedThumbFileLocation, "Image");
+//                                            String uploadedThumbFileLocation = writeThumbnailImage(uploadedFileLocationToWrite);
+                                            attachmentId = attachmentDao.insert(uploadedFileLocationToWrite, "", "Image");
                                             gotAttachment = true;
                                         }
 
